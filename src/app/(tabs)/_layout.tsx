989f62c31af623d063aeacc5212f040a1e56225a
emekathegreat <emekamba10@gmail.com>
2024-06-17 13:49:49 -0400

    profile screen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -36,6 +36,15 @@ export default function TabLayout() {
             ),
           }}
         />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+            ),
+          }}
+        />
     </Tabs>
   );
 }
