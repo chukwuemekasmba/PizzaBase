e7fab7cf7c5a4e69c938e7459bba20c06ea966ed
emekathegreat <emekamba10@gmail.com>
2024-07-05 15:50:19 -0400

    query provider with react-query

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -1,9 +1,24 @@
-import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, FlatList } from 'react-native';
+
 import products from '@/assets/data/products';
+import { supabase } from "@/src/lib/supabase";
 
 import ProductListItem from "@/src/components/ProductListItem";
 
 export default function HomeScreen() {
+  const [productsList, setProductsList] = useState();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data, error } = await supabase.from('products').select('*');
+
+      // setProductsList(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
       <FlatList
         data={products}
