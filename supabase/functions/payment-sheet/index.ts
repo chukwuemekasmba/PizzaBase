209785a054b9feb3c9850d908dc7f57b9deed48a
emekathegreat <emekamba10@gmail.com>
2024-07-25 16:42:42 -0400

    fixed payment sheet error

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,39 +1,32 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { stripe } from '../_utils/stripe';
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
 
-console.log('Hello from Functions!');
+// Setup type definitions for built-in Supabase Runtime APIs
+import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts"
 
-serve(async (req) => {
-  try {
-    const { amount } = await req.json();
+console.log("Hello from Functions!")
 
-    // Create a PaymentIntent so that the SDK can charge the logged in customer.
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 1099,
-      currency: 'usd',
-      // customer: customer,
-    });
-    const res = {
-      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
-      paymentIntent: paymentIntent.client_secret,
-      // ephemeralKey: ephemeralKey.secret,
-      // customer: customer,
-    };
-    return new Response(JSON.stringify(res), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 200,
-    });
-  } catch (error) {
-    return new Response(JSON.stringify(error), {
-      headers: { 'Content-Type': 'application/json' },
-      status: 400,
-    });
+Deno.serve(async (req) => {
+  const { name } = await req.json()
+  const data = {
+    message: `Hello ${name}!`,
   }
-});
 
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
 
-// To invoke:
-// curl -i --request POST 'http://localhost:54321/functions/v1/payment-sheet' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"amount":1150}'
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
