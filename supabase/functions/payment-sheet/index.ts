9584de8b7e3d3f0c33f200ac6e9f3210c6349c6b
emekathegreat <emekamba10@gmail.com>
2024-07-26 16:51:06 -0400

    successfully deployed payment sheet to supabase

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,8 +1,40 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import { stripe } from '@/supabase/functions/_utils/stripe';
 
-// Setup type definitions for built-in Supabase Runtime APIs
+console.log('Hello from Functions!');
+
+serve(async (req) => {
+  try {
+    const { amount } = await req.json();
+
+    // Create a PaymentIntent so that the SDK can charge the logged in customer.
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: 'usd',
+      // customer: customer,
+    });
+    const res = {
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+      paymentIntent: paymentIntent.client_secret,
+      // ephemeralKey: ephemeralKey.secret,
+      // customer: customer,
+    };
+    return new Response(JSON.stringify(res), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
+});
+
+/* Setup Guide */
+
+/* 
+
 import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts"
 
 console.log("Hello from Functions!")
@@ -17,7 +49,16 @@ Deno.serve(async (req) => {
     JSON.stringify(data),
     { headers: { "Content-Type": "application/json" } },
   )
-})
+});
+
+*/
+
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+
 
 /* To invoke locally:
 
