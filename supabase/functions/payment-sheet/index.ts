f1f9f9cd290473dbe199a3bde467c1d7c1afca7d
emekathegreat <emekamba10@gmail.com>
2024-07-26 19:52:18 -0400

    payment sheet functions

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
-import { stripe } from '@/supabase/functions/_utils/stripe';
+import { stripe } from '../_utils/stripe.ts';
 
 console.log('Hello from Functions!');
 
@@ -65,7 +65,7 @@ Deno.serve(async (req) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
+  curl -i --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
