5791777b2f4972f73fbef76b69e5c2367b580297
emekathegreat <emekamba10@gmail.com>
2024-08-29 00:29:45 -0400

    stripe payment sheet config

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,11 +1,12 @@
 import { Alert } from 'react-native';
 import { supabase } from './supabase';
-// import { useStripe } from '@stripe/stripe-react-native';
+import { initPaymentSheet, presentPaymentSheet } from '@stripe/stripe-react-native';
 
-export const fetchPaymentSheetParams = async () => {
+export const fetchPaymentSheetParams = async (amount: number, currency: string ) => {
   const { data, error } = await supabase.functions.invoke(
-    "payment-sheet",
-  );
+    "payment-sheet", {
+      body: { amount: amount, currency: currency }
+    });
 
   console.log(data, error);
 
@@ -13,12 +14,36 @@ export const fetchPaymentSheetParams = async () => {
     Alert.alert(`Error: ${error?.message ?? "no data"}`);
     return {};
   }
-  const { paymentIntent, ephemeralKey, customer, stripe_pk } = data;
+};
 
-  return {
-    paymentIntent,
-    ephemeralKey,
-    customer,
-    stripe_pk,
-  };
+
+export const initialisePaymentSheet = async (
+  amount: number,
+  currency: string,
+  name: string = "John Doe",
+) => {
+  const { paymentIntent, publishableKey } = await fetchPaymentSheetParams(
+    amount,
+    currency,
+  );
+
+  if (!paymentIntent || !publishableKey) return;
+
+  await initPaymentSheet({
+    merchantDisplayName: "ExpoDash",
+    paymentIntentClientSecret: paymentIntent,
+    returnURL: "your-app://stripe-redirect",
+    defaultBillingDetails: {
+      name: name,
+    },
+  });
+};
+
+export const openPaymentSheet = async () => {
+  const { error } = await presentPaymentSheet();
+
+  if (error) {
+    return false;
+  }
+  return true;
 };
