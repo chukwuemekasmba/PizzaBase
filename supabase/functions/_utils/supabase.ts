eef6c5d4072cc1df56fcfa9451858efca4a304c7
emekathegreat <emekamba10@gmail.com>
2024-08-28 23:26:43 -0400

    supabase functions edit

--- supabase/functions/_utils/supabase.ts
+++ supabase/functions/_utils/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { stripe } from "./stripe.ts";
 
 export const createOrRetrieveProfile = async (req: Request) => {
     const supabaseClient = createClient(
@@ -17,4 +18,30 @@ export const createOrRetrieveProfile = async (req: Request) => {
 
     console.log(user);
 		if (!user) throw new Error('No user found');
+
+    const { data: profile, error } = await supabaseClient
+      .from("profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single();
+
+    if (error || !profile) {
+      throw new Error("Profile not found");
+    }
+
+    if (profile.stripe_customer_id) {
+      return profile.stripe_customer_id;
+    }
+
+    const customer = await stripe.customers.create({
+      email: user.email,
+      metadata: { uid: user.id }
+    });
+
+    await supabaseClient
+      .from("profiles")
+      .update({ stripe_customer_id: customer.id })
+      .eq("id", profile.id);
+
+    return customer.id;
 };
--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,45 +1,59 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { stripe } from '../_utils/stripe.ts';
-import { createOrRetrieveCustomer } from "../_utils/stripe.ts";
+import { stripe, corsHeaders } from '../_utils/stripe.ts';
+import { createOrRetrieveProfile } from "../_utils/supabase.ts";
+
+console.log("payment-sheet handler up and running!");
 
 serve(async (req) => {
-  try {
-    const authHeader = req.headers.get("Authorization")!;
-    
-    const customer = await createOrRetrieveCustomer(authHeader);
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
 
+  try {    
+    const { amount, currency }: { amount: number; currency: string } = 
+      await req.json();
+    const customer = await createOrRetrieveProfile(req);
     const ephemeralKey = await stripe.ephemeralKeys.create(
-      { customer: customer }
+      { customer: customer },
+      { apiVersion: "2020-08-27"}
     );
 
-    const { amount } = await req.json();
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'usd',
       customer: customer,
     });
+
     const res = {
       publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
       paymentIntent: paymentIntent.client_secret,
-      ephemeralKey: ephemeralKey.secret,
       customer: customer,
+      ephemeralKey: ephemeralKey.secret,
     };
+
     return new Response(JSON.stringify(res), {
-      headers: { 'Content-Type': 'application/json' },
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 200,
     });
+    
   } catch (error) {
     return new Response(JSON.stringify(error), {
-      headers: { 'Content-Type': 'application/json' },
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 400,
     });
   }
 });
 
 
-// To invoke:
-// curl -i --request POST 'http://localhost:54321/functions/v1/payment-sheet' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"amount": 1150 }'
+/*  To invoke locally:
+
+1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+2. Make an HTTP request:
+
+   curl -i --request POST 'http://localhost:54321/functions/v1/payment-sheet' \
+     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+     --header 'Content-Type: application/json' \
+     --data '{"amount": 1150, "currency": "usd" }'
+
+*/
